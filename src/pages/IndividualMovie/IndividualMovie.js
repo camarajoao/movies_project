d8a55aa5a71f73c4debc90de9114a8a1131aee81
Thais Campos <thaismca.bio@gmail.com>
2023-06-09 15:30:33 -0700

    adjusted list of recommendations to reuse the ListOfMovies

--- src/pages/IndividualMovie/IndividualMovie.js
+++ src/pages/IndividualMovie/IndividualMovie.js
@@ -9,7 +9,7 @@ import MovieAdditionalDetails from '../../components/MovieAdditionalDetails/Movi
 import MovieOverview from '../../components/MovieOverview/MovieOverview';
 import TopCast from '../../components/TopCast/TopCast';
 import MovieDetails from '../../components/MovieDetails/MovieDetails';
-import Recommendations from '../../components/Recommendations/Recommendations';
+import ListOfMovies from '../../components/ListOfMovies/ListOfMovies';
 import Footer from '../../components/Footer/Footer';
 
 import { getRequestParams, getDataFromAPI } from "../../helpers/utils";
@@ -21,31 +21,36 @@ function IndividualMovie({ theme }) {
     // states for data retrieved from API
     const [movieDetails, setMovieDetails] = useState(null);
     const [topCast, setTopCast] = useState(null);
+    const [recommended, setRecommended] = useState(null)
 
     // getting movie data based on id in params
     const { movieId } = useParams();
     const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`
     const topCastUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`
+    const recommendedUrl = `https://api.themoviedb.org/3/movie/${movieId}/recommendations?language=en-US&page=1`
     
     // setting the request params for the API endpoints
     const movieDetailsParams = getRequestParams(movieDetailsUrl);
-    const topCastParams = getRequestParams(topCastUrl)
+    const topCastParams = getRequestParams(topCastUrl);
+    const recommendedParams = getRequestParams(recommendedUrl);
 
     // function below triggers the helper function
-    const getMovieDetails = () => getDataFromAPI(movieDetailsParams, setMovieDetails)
-    const getTopCast = () => getDataFromAPI(topCastParams, setTopCast)
+    const getMovieDetails = () => getDataFromAPI(movieDetailsParams, setMovieDetails);
+    const getTopCast = () => getDataFromAPI(topCastParams, setTopCast);
+    const getRecommended = () => getDataFromAPI(recommendedParams, setRecommended)
 
     // this runs the getData trigger function as useEffect
     useEffect(() => {
         getMovieDetails()
         getTopCast()
+        getRecommended()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    if (!movieDetails || !topCast) {
+    if (!movieDetails || !topCast || !recommended) {
         return
     };
-
+console.log(recommended)
     return (
         <div className='individual-movie'>
             <MovieHeader movieDetails={movieDetails} />
@@ -54,7 +59,7 @@ function IndividualMovie({ theme }) {
             <MovieOverview movieDetails={movieDetails} />
             <TopCast theme={theme} imageBaseUrl={imageBaseUrl} cast={topCast.cast} />
             <MovieDetails movieDetails={movieDetails} crew={topCast.crew} />
-            <Recommendations theme={theme} imageBaseUrl={imageBaseUrl} />
+            <ListOfMovies imagesBaseUrl={imageBaseUrl} movies={recommended.results} sectionTitle={"You may also like"} theme={theme} />
             <Footer theme={theme} />
         </div>
     )
